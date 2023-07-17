2d8dfa6582e20830531a21c7c9eff535156dd46d
Andrii Danylkov <danilkovxp@gmail.com>
2023-07-17 23:55:05 +0200

    feat: add food page using useQuery

--- src/FoodSense.Client/src/main.tsx
+++ src/FoodSense.Client/src/main.tsx
@@ -1,22 +1,31 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom"
-import { AddFood } from './Pages/FoodPage';
+import { AddFood } from './Pages/AddFood';
 import React from 'react';
 import { Notifications } from '@mantine/notifications';
+import { FoodPage } from './Pages/FoodPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
     {
         path: "/add",
         element: <AddFood />
+    },
+    {
+        path: "/",
+        element: <FoodPage />
     }]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Notifications />
-        <RouterProvider router={router} />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <React.StrictMode>
+            <Notifications />
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    </QueryClientProvider>
 );
