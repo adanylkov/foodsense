f8a1e580c69b9a64be4eadbb0dbfad8e6fa1e7d0
Andrii Danylkov <danilkovxp@gmail.com>
2023-07-19 22:48:33 +0200

    feat: split food element into smaller components

--- src/FoodSense.Client/src/Pages/FoodElement.tsx
+++ src/FoodSense.Client/src/Pages/FoodElement.tsx
@@ -1,7 +1,78 @@
-import { Card, Image, Text, Badge, Group, Accordion, Flex } from '@mantine/core';
+import { Card, Image, Text, Badge, Group, Accordion, Flex, Menu, ActionIcon } from '@mantine/core';
 import { api_path } from '../api';
-import { IconFlame, IconDroplet, IconGrowth, IconMeat } from '@tabler/icons-react';
+import { IconFlame, IconDroplet, IconGrowth, IconMeat, IconSettings, IconMessageCircle, IconPhoto, IconSearch, IconArrowsLeftRight, IconTrash } from '@tabler/icons-react';
 
+interface NutritionInfoProps {
+    calories: number;
+    fat: number;
+    carbohydrates: number,
+    protein: number,
+    sodium: number,
+    sugar: number,
+    fiber: number,
+    cholesterol: number,
+    saturatedFat: number,
+    transFat: number,
+    unsaturatedFat: number,
+    potassium: number,
+}
+
+const NutritionInfo = (props: NutritionInfoProps) => {
+    return (
+        <Accordion variant='filled' radius={'md'}>
+            <Accordion.Item value="macronutrients">
+                <Accordion.Control>Macronutrients</Accordion.Control>
+                <Accordion.Panel>
+                    <Flex gap={'xs'}>
+                        <IconFlame />
+                        <Text weight={500}>Calories:</Text>
+                        <Text>{props.calories}</Text>
+                    </Flex>
+                    <Flex gap={'xs'}>
+                        <IconDroplet />
+                        <Text weight={500}>Fat:</Text>
+                        <Text>{props.fat}</Text>
+                    </Flex>
+                    <Flex gap={'xs'}>
+                        <IconGrowth />
+                        <Text weight={500}>Carbohydrates:</Text>
+                        <Text>{props.carbohydrates}</Text>
+                    </Flex>
+                    <Flex gap={'xs'}>
+                        <IconMeat />
+                        <Text weight={500}>Proteins:</Text>
+                        <Text>{props.protein}</Text>
+                    </Flex>
+                </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item value="micronutrients">
+                <Accordion.Control>Micronutrients</Accordion.Control>
+                <Accordion.Panel>TODO</Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item value="other">
+                <Accordion.Control>Other</Accordion.Control>
+                <Accordion.Panel>TODO</Accordion.Panel>
+            </Accordion.Item>
+        </Accordion>
+    )
+}
+
+const ControlMenu = () => {
+    return (
+        <Menu>
+            <Menu.Target>
+                <ActionIcon variant="subtle"><IconSettings size="1.5rem" /></ActionIcon>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+                <Menu.Label>Controls</Menu.Label>
+                <Menu.Item icon={<IconTrash size={14} color='red' />}>Delete food</Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
+    )
+}
 interface FoodElementProps {
     name: string;
     imageUrl: string;
@@ -28,54 +99,16 @@ export const FoodElement = (props: FoodElementProps) => {
             <Card.Section>
                 <Image
                     src={`${api_path}/images/${props.imageUrl}`}
-                    height={160}
+                    height={200}
                 />
             </Card.Section>
 
             <Group position="apart" mt="md" mb="xs">
                 <Text weight={500}>{props.name}</Text>
-                <Badge color="pink" variant="light">
-                    On Sale
-                </Badge>
+                <ControlMenu />
             </Group>
 
-            <Accordion>
-                <Accordion.Item value="macronutrients">
-                    <Accordion.Control>Macronutrients</Accordion.Control>
-                    <Accordion.Panel>
-                        <Flex gap={'xs'}>
-                            <IconFlame />
-                            <Text weight={500}>Calories:</Text>
-                            <Text>{props.nutrition.calories}</Text>
-                        </Flex>
-                        <Flex gap={'xs'}>
-                            <IconDroplet />
-                            <Text weight={500}>Fat:</Text>
-                            <Text>{props.nutrition.fat}</Text>
-                        </Flex>
-                        <Flex gap={'xs'}>
-                            <IconGrowth />
-                            <Text weight={500}>Carbohydrates:</Text>
-                            <Text>{props.nutrition.carbohydrates}</Text>
-                        </Flex>
-                        <Flex gap={'xs'}>
-                            <IconMeat />
-                            <Text weight={500}>Proteins:</Text>
-                            <Text>{props.nutrition.protein}</Text>
-                        </Flex>
-                    </Accordion.Panel>
-                </Accordion.Item>
-
-                <Accordion.Item value="micronutrients">
-                    <Accordion.Control>Micronutrients</Accordion.Control>
-                    <Accordion.Panel>TODO</Accordion.Panel>
-                </Accordion.Item>
-
-                <Accordion.Item value="other">
-                    <Accordion.Control>Other</Accordion.Control>
-                    <Accordion.Panel>TODO</Accordion.Panel>
-                </Accordion.Item>
-            </Accordion>
+            <NutritionInfo {...props.nutrition} />
         </Card>
     );
 }
