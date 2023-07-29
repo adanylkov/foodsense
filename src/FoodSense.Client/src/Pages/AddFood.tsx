ea25b3321c68a6bc433d90f90c9059e0395f464e
Andrii Danylkov <danilkovxp@gmail.com>
2023-07-30 00:01:53 +0200

    feat: interactively show expiration or new food page on add

--- src/FoodSense.Client/src/Pages/AddFood.tsx
+++ src/FoodSense.Client/src/Pages/AddFood.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { FoodForm } from '../Components/FoodForm';
 import { BarcodeScanner } from './BarcodeScanner';
+import { useQuery } from '@tanstack/react-query';
+import { api_path } from '../api';
+import { AddExpiration } from './AddExpiration';
+import { Center, Loader } from '@mantine/core';
 
 export const AddFood = () => {
 
@@ -12,5 +16,26 @@ export const AddFood = () => {
     setBarcode(result);
   }
 
-  return showScanner ? <BarcodeScanner onDetected={onDetected} /> : <FoodForm barcode={barcode} />
+  const { isLoading, isError } = useQuery(["food", barcode], {
+    enabled: !!barcode,
+    queryFn: async () => {
+      const response = await fetch(`${api_path}/api/Food/${barcode}`);
+      return await response.json();
+    }
+  });
+
+  if (!isLoading && !isError && barcode) {
+    return <AddExpiration barcode={barcode} />
+  }
+  else if (!isLoading && isError && barcode) {
+    return <FoodForm barcode={barcode} />
+  }
+  else if (isLoading && barcode) {
+    return (
+      <Center h={'100vh'}>
+        <Loader />
+      </Center>
+    )
+  }
+  return <BarcodeScanner onDetected={onDetected} />
 }
